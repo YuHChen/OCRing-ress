b8f542ad1409382e42851a5c60d86f50f0a5c536
Yu Heng Chen <yuhchen21@gmail.com>
2016-02-18 17:05:18 -0500

    Experiment with Project Oxford output.

---
+++ test/oxfordjson.js
@@ -0,0 +1,573 @@
+var ox_out = {
+  "language": "en",
+  "textAngle": 0,
+  "orientation": "Up",
+  "regions": [
+    {
+      "boundingBox": "156,30,473,24",
+      "lines": [
+        {
+          "boundingBox": "156,30,83,24",
+          "words": [
+            {
+              "boundingBox": "156,30,83,24",
+              "text": "jTORE"
+            }
+          ]
+        },
+        {
+          "boundingBox": "274,30,94,24",
+          "words": [
+            {
+              "boundingBox": "274,30,94,24",
+              "text": "AGENT"
+            }
+          ]
+        },
+        {
+          "boundingBox": "402,30,227,24",
+          "words": [
+            {
+              "boundingBox": "402,30,75,23",
+              "text": "INTEL"
+            },
+            {
+              "boundingBox": "513,30,116,24",
+              "text": "MISSION"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "boundingBox": "2,120,361,913",
+      "lines": [
+        {
+          "boundingBox": "27,120,285,47",
+          "words": [
+            {
+              "boundingBox": "27,134,49,23",
+              "text": "ALL"
+            },
+            {
+              "boundingBox": "83,134,65,23",
+              "text": "TIME"
+            },
+            {
+              "boundingBox": "178,120,3,47",
+              "text": "I"
+            },
+            {
+              "boundingBox": "211,134,101,24",
+              "text": "MONTH"
+            }
+          ]
+        },
+        {
+          "boundingBox": "3,197,138,31",
+          "words": [
+            {
+              "boundingBox": "3,197,138,31",
+              "text": "Discovery"
+            }
+          ]
+        },
+        {
+          "boundingBox": "3,242,306,30",
+          "words": [
+            {
+              "boundingBox": "3,243,98,29",
+              "text": "Unique"
+            },
+            {
+              "boundingBox": "110,243,98,24",
+              "text": "Portals"
+            },
+            {
+              "boundingBox": "215,242,94,25",
+              "text": "Visited"
+            }
+          ]
+        },
+        {
+          "boundingBox": "2,282,182,25",
+          "words": [
+            {
+              "boundingBox": "2,283,41,23",
+              "text": "XM"
+            },
+            {
+              "boundingBox": "53,282,131,25",
+              "text": "Collected"
+            }
+          ]
+        },
+        {
+          "boundingBox": "3,376,231,25",
+          "words": [
+            {
+              "boundingBox": "3,377,122,24",
+              "text": "Distance"
+            },
+            {
+              "boundingBox": "133,376,101,25",
+              "text": "Walked"
+            }
+          ]
+        },
+        {
+          "boundingBox": "3,424,115,32",
+          "words": [
+            {
+              "boundingBox": "3,424,115,32",
+              "text": "Buildinq"
+            }
+          ]
+        },
+        {
+          "boundingBox": "3,470,298,30",
+          "words": [
+            {
+              "boundingBox": "3,471,160,24",
+              "text": "Resonators"
+            },
+            {
+              "boundingBox": "172,470,129,30",
+              "text": "Deployed"
+            }
+          ]
+        },
+        {
+          "boundingBox": "3,510,189,25",
+          "words": [
+            {
+              "boundingBox": "3,511,72,24",
+              "text": "Links"
+            },
+            {
+              "boundingBox": "84,510,108,25",
+              "text": "Created"
+            }
+          ]
+        },
+        {
+          "boundingBox": "3,550,308,25",
+          "words": [
+            {
+              "boundingBox": "3,551,101,24",
+              "text": "Control"
+            },
+            {
+              "boundingBox": "114,550,80,25",
+              "text": "Fields"
+            },
+            {
+              "boundingBox": "203,550,108,25",
+              "text": "Created"
+            }
+          ]
+        },
+        {
+          "boundingBox": "3,590,284,30",
+          "words": [
+            {
+              "boundingBox": "3,590,66,25",
+              "text": "Mind"
+            },
+            {
+              "boundingBox": "79,591,72,24",
+              "text": "Units"
+            },
+            {
+              "boundingBox": "160,590,127,30",
+              "text": "Captured"
+            }
+          ]
+        },
+        {
+          "boundingBox": "3,630,360,30",
+          "words": [
+            {
+              "boundingBox": "3,631,111,29",
+              "text": "Longest"
+            },
+            {
+              "boundingBox": "123,631,56,24",
+              "text": "Link"
+            },
+            {
+              "boundingBox": "188,631,58,24",
+              "text": "Ever"
+            },
+            {
+              "boundingBox": "255,630,108,25",
+              "text": "Created"
+            }
+          ]
+        },
+        {
+          "boundingBox": "3,670,286,30",
+          "words": [
+            {
+              "boundingBox": "3,671,103,29",
+              "text": "Largest"
+            },
+            {
+              "boundingBox": "115,671,101,24",
+              "text": "Control"
+            },
+            {
+              "boundingBox": "226,670,63,25",
+              "text": "Field"
+            }
+          ]
+        },
+        {
+          "boundingBox": "2,710,201,30",
+          "words": [
+            {
+              "boundingBox": "2,711,41,23",
+              "text": "XM"
+            },
+            {
+              "boundingBox": "53,710,150,30",
+              "text": "Recharged"
+            }
+          ]
+        },
+        {
+          "boundingBox": "3,750,234,30",
+          "words": [
+            {
+              "boundingBox": "3,751,98,24",
+              "text": "Portals"
+            },
+            {
+              "boundingBox": "110,750,127,30",
+              "text": "Captured"
+            }
+          ]
+        },
+        {
+          "boundingBox": "3,790,341,30",
+          "words": [
+            {
+              "boundingBox": "3,791,98,29",
+              "text": "Unique"
+            },
+            {
+              "boundingBox": "110,791,98,24",
+              "text": "Portals"
+            },
+            {
+              "boundingBox": "217,790,127,30",
+              "text": "Captured"
+            }
+          ]
+        },
+        {
+          "boundingBox": "3,830,213,30",
+          "words": [
+            {
+              "boundingBox": "3,830,75,25",
+              "text": "Mods"
+            },
+            {
+              "boundingBox": "87,830,129,30",
+              "text": "Deployed"
+            }
+          ]
+        },
+        {
+          "boundingBox": "3,878,110,27",
+          "words": [
+            {
+              "boundingBox": "3,878,110,27",
+              "text": "Combat"
+            }
+          ]
+        },
+        {
+          "boundingBox": "3,923,310,30",
+          "words": [
+            {
+              "boundingBox": "3,924,160,24",
+              "text": "Resonators"
+            },
+            {
+              "boundingBox": "172,923,141,30",
+              "text": "Destroyed"
+            }
+          ]
+        },
+        {
+          "boundingBox": "3,963,264,25",
+          "words": [
+            {
+              "boundingBox": "3,964,98,24",
+              "text": "Portals"
+            },
+            {
+              "boundingBox": "110,963,157,25",
+              "text": "Neutralized"
+            }
+          ]
+        },
+        {
+          "boundingBox": "3,1003,323,30",
+          "words": [
+            {
+              "boundingBox": "3,1004,92,29",
+              "text": "Enemy"
+            },
+            {
+              "boundingBox": "104,1004,72,24",
+              "text": "Links"
+            },
+            {
+              "boundingBox": "185,1003,141,30",
+              "text": "Destroyed"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "boundingBox": "376,120,236,618",
+      "lines": [
+        {
+          "boundingBox": "484,120,101,47",
+          "words": [
+            {
+              "boundingBox": "484,120,3,47",
+              "text": "I"
+            },
+            {
+              "boundingBox": "517,134,68,24",
+              "text": "NOW"
+            }
+          ]
+        },
+        {
+          "boundingBox": "376,134,79,23",
+          "words": [
+            {
+              "boundingBox": "376,134,79,23",
+              "text": "WEEK"
+            }
+          ]
+        },
+        {
+          "boundingBox": "577,242,35,25",
+          "words": [
+            {
+              "boundingBox": "577,242,35,25",
+              "text": "50"
+            }
+          ]
+        },
+        {
+          "boundingBox": "456,282,155,28",
+          "words": [
+            {
+              "boundingBox": "456,282,106,28",
+              "text": "466,183"
+            },
+            {
+              "boundingBox": "570,283,41,24",
+              "text": "XM"
+            }
+          ]
+        },
+        {
+          "boundingBox": "550,377,61,24",
+          "words": [
+            {
+              "boundingBox": "550,377,13,24",
+              "text": "7"
+            },
+            {
+              "boundingBox": "571,377,40,24",
+              "text": "km"
+            }
+          ]
+        },
+        {
+          "boundingBox": "578,471,33,24",
+          "words": [
+            {
+              "boundingBox": "578,471,33,24",
+              "text": "58"
+            }
+          ]
+        },
+        {
+          "boundingBox": "585,511,26,24",
+          "words": [
+            {
+              "boundingBox": "585,511,26,24",
+              "text": "19"
+            }
+          ]
+        },
+        {
+          "boundingBox": "585,551,26,24",
+          "words": [
+            {
+              "boundingBox": "585,551,26,24",
+              "text": "19"
+            }
+          ]
+        },
+        {
+          "boundingBox": "496,590,116,25",
+          "words": [
+            {
+              "boundingBox": "496,590,45,25",
+              "text": "108"
+            },
+            {
+              "boundingBox": "550,591,62,24",
+              "text": "Ml-is"
+            }
+          ]
+        },
+        {
+          "boundingBox": "545,630,66,25",
+          "words": [
+            {
+              "boundingBox": "545,630,17,25",
+              "text": "O"
+            },
+            {
+              "boundingBox": "571,631,40,24",
+              "text": "km"
+            }
+          ]
+        },
+        {
+          "boundingBox": "514,670,98,25",
+          "words": [
+            {
+              "boundingBox": "514,670,28,25",
+              "text": "27"
+            },
+            {
+              "boundingBox": "550,671,62,24",
+              "text": "Ml-is"
+            }
+          ]
+        },
+        {
+          "boundingBox": "462,710,149,28",
+          "words": [
+            {
+              "boundingBox": "462,710,100,28",
+              "text": "365,127"
+            },
+            {
+              "boundingBox": "570,711,41,24",
+              "text": "XM"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "boundingBox": "570,750,42,318",
+      "lines": [
+        {
+          "boundingBox": "595,750,17,25",
+          "words": [
+            {
+              "boundingBox": "595,750,17,25",
+              "text": "6"
+            }
+          ]
+        },
+        {
+          "boundingBox": "595,790,17,25",
+          "words": [
+            {
+              "boundingBox": "595,790,17,25",
+              "text": "6"
+            }
+          ]
+        },
+        {
+          "boundingBox": "580,830,32,25",
+          "words": [
+            {
+              "boundingBox": "580,830,32,25",
+              "text": "23"
+            }
+          ]
+        },
+        {
+          "boundingBox": "570,923,42,25",
+          "words": [
+            {
+              "boundingBox": "570,923,42,25",
+              "text": "123"
+            }
+          ]
+        },
+        {
+          "boundingBox": "587,963,25,25",
+          "words": [
+            {
+              "boundingBox": "587,963,25,25",
+              "text": "12"
+            }
+          ]
+        },
+        {
+          "boundingBox": "578,1003,34,25",
+          "words": [
+            {
+              "boundingBox": "578,1003,34,25",
+              "text": "26"
+            }
+          ]
+        },
+        {
+          "boundingBox": "587,1043,25,25",
+          "words": [
+            {
+              "boundingBox": "587,1043,25,25",
+              "text": "13"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "boundingBox": "3,1043,442,30",
+      "lines": [
+        {
+          "boundingBox": "3,1043,442,30",
+          "words": [
+            {
+              "boundingBox": "3,1044,92,29",
+              "text": "Enemy"
+            },
+            {
+              "boundingBox": "104,1044,101,24",
+              "text": "Control"
+            },
+            {
+              "boundingBox": "215,1043,80,25",
+              "text": "Fields"
+            },
+            {
+              "boundingBox": "304,1043,141,30",
+              "text": "Destroyed"
+            }
+          ]
+        }
+      ]
+    }
+  ]
+};
+
+console.log("num regions =", ox_out.regions.length);
